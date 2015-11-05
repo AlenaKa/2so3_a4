196054a9fe5a5a62ff5f689a284c7658c0f6103a
AlenaKa <magdalena.karski@gmail.com>
2015-11-04 21:18:04 -0500

    base

---
+++ src/User.java
@@ -0,0 +1,7 @@
+
+public class User {
+	private String username;
+	public String getUsername(...) { // stores the username.
+	// usernames of all registered users are stored in the file Users.txt
+	}
+}
